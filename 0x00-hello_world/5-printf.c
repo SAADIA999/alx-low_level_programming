7508018c1093245cc78a1c37bb62afe64f34c588
SAADIA999 <saadia.moussaoui60@gmail.com>
2023-02-16 13:57:35 -0800

    Write a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
-    printf("In the depths of winter, I finally learned that within me there lay an invincible summer.\n");
-    return 0;
+int main(void){
+printf("In the depths of winter, I finally learned that within me there lay an invincible summer.\n");
+return 0;
 }
