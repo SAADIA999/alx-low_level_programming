ae850ebfab23927403a86e2c59a7fb10dbacf0a4
SAADIA999 <saadia.moussaoui60@gmail.com>
2023-03-01 02:14:21 -0800

    Task-0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include<stdlib.h>
+
+/**
+ * _strcat - concatenate Two Strings
+ *
+ * @str: string to check
+ * Return: nothing
+ */
+
+void _strcat(char *str1,char *str2)
+{
+	print(
+
+			}
