2ecbe3dc66f62ac74f2eab1e672f9b9304965541
SAADIA999 <saadia.moussaoui60@gmail.com>
2023-03-09 09:10:40 -0800

    KSJHDFDJKHWUH

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,15 +3,15 @@
  *_strlen_recursion - Returns the length of a string.
  *@s: The String to be used.
  *
- *Return :The length of the string.
+ *Return: The length of the string.
  */
 int _strlen_recursion(char *s)
 {
-	int cnt;
+	int cnt = 0;
 
 	if (*s)
 	{
-		cnt++;
+		cnt = cnt + 1;
 		cnt += _strlen_recursion(s + 1);
 	}
 	return (cnt);
